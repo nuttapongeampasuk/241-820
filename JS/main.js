f2e6ed4b5270aadb13a774b11e0d6de2bd8fd15f
nuttapongeampasuk <nuttapong.e@ku.th>
2026-01-05 17:00:30 +0700

    js update

--- JS/main.js
+++ JS/main.js
@@ -1,105 +1,132 @@
 /*
-// String, Number, Boolean, Object, Array
-
-//1. String
-let name = "John Doe";
-const idcard ='123';
-
-//2. Number
-let age = 30;
-let height = 5.9;
-
-//3. Boolean
-let isStudent = false;
-firstName = 'test';
-console.log('My name is', firstName, 'and I am', age, 'years old.');
-
-/*
-+ บวก 
-- ลบ
-* คูณ
-/ หาร
-% หารเอาเศษ mod
+array
 */
 
-/*let number1 = 10;
-let number2 = 5;
+/*let a = 10
+let b = 20
+let c = 30
+console.log('a:', a, 'b:', b, 'c:', c)
 
-let result = number1 + number2;
-console.log("ผลลบ =", result);*/
+let ages = [10,20,30]
+console.log('ages:', ages)
+console.log('ages[1]:', ages[1])
+
+//1.แทนที่ ค่าข้อมูลใน array
+ages = [15,25]
+console.log('ages list:',ages)
+//2. ต่อ array
+ages.push(35)
+console.log('ages after push:', ages)
+//3. ลบข้อมูลตัวสุดท้ายใน array
+ages.pop()
+console.log('ages after pop:', ages)*/
+
+/*let ages = [25,30,35,40,45]
+console.log('Ages:',ages)
+
+let fruits =['apple','banana','cherry']
+console.log("Fruits:",fruits)
+fruits.push('mango')
+console.log("Fruits after push:",fruits)
+console.log('First fruit:', fruits.length)
+
+for(let i = 0; i < fruits.length; i++) {
+    console.log(`Fruits at index ${i}:`/fruits[i])
+}
+/*
+object
+*/
+/*let student = [{
+    age:30,
+    name:"John",
+    grade: 'A'
+}, {
+    age: 25,
+    name: "Jane",
+    grade: 'B'
+}]
+for (let i = 0; i< student.length; i++) {
+    console.log("student", + (i+1)+":",);
+    console.log('name:', student[i].name);
+    console.log('name:', student[i].age);
+    console.log('name:', student[i].grade);
+}
+student.push({
+    age : 28,
+    name: 'Mike',
+    grade : 'C'
+}) ;
+console.log ("Added new student:" , student[student.length - 1]) ;
+student.pop() ;
+console.log ("Removed last student. Current students:", student) ;
 
 /*
-== เท่ากับ
-!= ไม่เท่ากับ
-> น้อยกว่า
-< มากกว่า
->= น้อยกว่าหรือเท่ากับ
-<= มากกว่าหรือเท่ากับ
+function
 */
 
-/*let number1 = 25;
-let number2 = 25;
+/*let score1 = 10
+let score2 = 80
 
-/*let condition1 = number == number2; //Boolean ค่า true หรือ false
-console.log("Result of condition is :", condition1);*/
-
-//if-else condition
-/*if (number1 != number2) {
-    console.log('this if')
-} else if (number1 == number2) {
-    console.log('this else if')
-} else {
-    console.log('this else')
-}*/
-
-/*
-Grade
->= 80 เป็นเกรด A
->= 70 เป็นเกรด B
->= 60 เป็นเกรด C
->= 50 เป็นเกรด D
-< 50 เป็นเกรด F
-*/
-/*let score = prompt("ป้อนคะแนนของคุณ")
-console.log("คะแนนของคุณ "+ score)
-
-if (score >= 80) {
-    console.log('A')
-} else if (score >= 70) {
-    console.log('B')
-} else if (score >= 60) {
-    console.log('C')
-} else if (score >= 50) {
-    console.log('D')
-} else {
-    console.log('F')
-}*/
-
-/*let number1 = 5
-let number2 = 8
-// true && false
-let condition = !(number1 >= 3 || number2 >= 10);
-console.log("condition:", condition);
-
-let age = 30;
-let gender = "ชาย";
-
-if (age >= 20 && gender == "ชาย") {
-    console.log("ผู้ชายที่มีอายุ 20 ปีขึ้นไป");
-}*/
-
-/*
-loop statements:
-while
-for
-*/
-let counter = 0;
-while(counter < 10) {
-    console.log('Hello World');
-    //counter = counter + 1;
-    counter += 1;
+function calculation_grade(score){
+    if (score >= 80 ) {
+        grade = 'A'
+    } else if (score >= 70){
+        grade = 'B'
+    } else if (score >= 60){
+        grade = 'C'
+    } else if (score >= 50) {
+        grade = 'D'
+    } else {
+        grade = 'F'
+    }
+    return grade
 }
 
-for(let i=0; i<10; i++) {
-    console.log('Hello World from for loop');
+//เรียกใช้ฟังก์ชัน calculation_grade เพื่อหาคะแนน
+let grade1 = calculation_grade(score1)
+let grade2 = calculation_grade(score2)
+console.log('Score1: ' + score1 + ', Grade: ' + grade1)
+console.log('Score2: ' + score2 + ', Grade: ' + grade2)
+
+/*
+array
+*/
+/*let scores = [90,80,70,60,50];
+
+for (let i = 0; i < score.length; i++) {
+    console.log(`Score ${i+1}: ${scores[i]}`);
 }
+
+scores = scores.map((s) => {
+    return s * 2;
+})
+score.forEach((s) => {
+   console.log('score:', s);
+   //console.log(`score: ${s}`); 
+})
+
+/*
+object functions
+*/
+let students = [
+    {name: "John", age: 20, grade: "A"},
+    {name: "Jane", age: 22, grade: "B"},
+    {name: "Jim", age: 21, grade: "C"}
+]
+console.log('Student',students[0]);
+
+let student = students.find((s) => {
+    return s.name === "Jane"
+})
+
+let dubblescoreStudents = students.map((s) => {
+    s.age = s.age * 2;
+    return s;
+});
+
+let highGradeStudents = students.filler((s) => {
+    return s.grade === "A";
+});
+console.log('Student ',student);
+console.log('Double Score Students',dubblescoreStudents);
+console.log('High Grade Students',highGradeStudents);
